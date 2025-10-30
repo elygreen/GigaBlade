235cc7fcd31cb69a7f655ccc070e4c5c5b597d23
elygreen <elycgreen@gmail.com>
2025-10-29 21:55:43 -0700

    Implemented Critical Hits
    - Added critical strikes
    - Added crit damage multipliers

--- Player/player_sword.gd
+++ Player/player_sword.gd
@@ -8,8 +8,12 @@ func _process(delta: float) -> void:
 
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	print("detected sword collision")
 	if area.owner and area.owner.is_in_group("enemy"):
-		print("area is enemy")
 		if area.owner.has_method("get_hit"):
-			area.owner.get_hit(PlayerStats.get_stat("sword_damage"))
+			var base_damage = PlayerStats.get_stat("sword_damage")
+			var total_damage = base_damage
+			var crit_chance = PlayerStats.get_stat("crit_chance")
+			if randf() * 100 < crit_chance:
+				var crit_mult = PlayerStats.get_stat("crit_modifier")
+				total_damage = base_damage + (base_damage * crit_mult)
+			area.owner.get_hit(total_damage)
