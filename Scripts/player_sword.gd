905229b5df24ce40e2323016058a0a1a95356b15
elygreen <elycgreen@gmail.com>
2025-10-30 23:41:16 -0700

    Fixed sword double damage bug
    - Changed sword hit detection from enemy hitbox.owner.is_in_group("enemy") to area.is_in_group("enemy_hurtbox")

--- Scripts/player_sword.gd
+++ Scripts/player_sword.gd
@@ -5,7 +5,7 @@ extends Node2D
 var counter = 0
 var is_attacking = false
 var parry_ready = false
-
+var enemits_hit_this_swing = []
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -27,14 +27,15 @@ func fire_projectile():
 	pass
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	if area.owner and area.owner.is_in_group("enemy"):
-		if area.owner.has_method("get_hit"):
-			var base_damage = PlayerStats.get_stat("sword_damage")
-			var total_damage = base_damage
-			var crit_chance = PlayerStats.get_stat("crit_chance")
-			var is_crit = false
-			if randf() * 100 < crit_chance:
-				var crit_mult = PlayerStats.get_stat("crit_modifier")
-				total_damage = base_damage + (base_damage * crit_mult)
-				is_crit = true
-			area.owner.get_hit(total_damage, is_crit)
+	if is_attacking:
+		if area.owner and area.is_in_group("enemy_hurtbox"):
+			if area.owner.has_method("get_hit"):
+				var base_damage = PlayerStats.get_stat("sword_damage")
+				var total_damage = base_damage
+				var crit_chance = PlayerStats.get_stat("crit_chance")
+				var is_crit = false
+				if randf() * 100 < crit_chance:
+					var crit_mult = PlayerStats.get_stat("crit_modifier")
+					total_damage = base_damage + (base_damage * crit_mult)
+					is_crit = true
+				area.owner.get_hit(total_damage, is_crit)
